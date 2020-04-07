186dba6ea934194f4310a2eb684ac94991af9311
Kumar, Sanjeet <sanjekumar@deloitte.com>
2020-04-07 19:58:11 +0530

    Updates

---
+++ precache-manifest.73a7017c97f356d0e924c83062180acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1406446bb9bfde64c2d952634725e5",
+    "url": "/pubg-tournament/index.html"
+  },
+  {
+    "revision": "72803b51f085a5c7808d",
+    "url": "/pubg-tournament/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "2be2fa289a0f5df277d1",
+    "url": "/pubg-tournament/static/css/main.cd68a29a.chunk.css"
+  },
+  {
+    "revision": "72803b51f085a5c7808d",
+    "url": "/pubg-tournament/static/js/2.24048c34.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pubg-tournament/static/js/2.24048c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be2fa289a0f5df277d1",
+    "url": "/pubg-tournament/static/js/main.e0384ca6.chunk.js"
+  },
+  {
+    "revision": "050d1e1a3faac1b53cab",
+    "url": "/pubg-tournament/static/js/runtime-main.4865f4ad.js"
+  }
+]);
